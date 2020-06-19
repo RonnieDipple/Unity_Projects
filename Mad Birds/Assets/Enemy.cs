4db23b9cbe8cc563bc3a541118768d11189fa357
Ronnie <ronniedipple@gmail.com>
2020-06-18 19:58:08 -0500

    onTouch added Cloud particles added
    Enemies now disappear on touch

---
+++ Mad Birds/Assets/Enemy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+
+    {
+        Bird bird = collision.collider.GetComponent<Bird>();
+        Enemy enemy = collision.collider.GetComponent<Enemy>();
+
+        if (bird != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        if (enemy != null)
+        {
+            return;
+        }
+
+        if (collision.contacts[0].normal.y < -0.5)
+        {
+            Destroy(gameObject);
+        }
+
+
+    }
+}
