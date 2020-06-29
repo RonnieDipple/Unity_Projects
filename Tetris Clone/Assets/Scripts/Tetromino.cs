831659eaf0acb6db9560d6208a7ceb4cec69b39a
Ronnie <ronniedipple@gmail.com>
2020-06-28 22:46:00 -0500

    Blocks fall
    Blocks they fall

--- Tetris Clone/Assets/Scripts/Tetromino.cs
+++ Tetris Clone/Assets/Scripts/Tetromino.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class Tetromino : MonoBehaviour
 {
+    private float fall = 0;
+    public float fullspeed = 1;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        checkUserInput();
+    }
+
+    void checkUserInput()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(1, 0, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(-1, 0, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+           transform.Rotate(0, 0, 90);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow) || Time.time - fall >= fullspeed)
+        {
+            transform.position += new Vector3(0, -1, 0);
+            fall = Time.time;
+        }
     }
 }
