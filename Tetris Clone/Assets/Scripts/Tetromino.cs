d91ad914964db1f336a8a3511d1869acc3a34e9e
Ronnie <ronniedipple@gmail.com>
2020-06-29 15:28:44 -0500

    Boundaries added
    Tetromino disappears on rotation near boundary

--- Tetris Clone/Assets/Scripts/Tetromino.cs
+++ Tetris Clone/Assets/Scripts/Tetromino.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class Tetromino : MonoBehaviour
 {
     private float fall = 0;
+
     public float fullspeed = 1;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,19 +24,61 @@ public class Tetromino : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             transform.position += new Vector3(1, 0, 0);
+            if (!CheckIsValidPosition())
+            {
+                transform.position += new Vector3(-1, 0,0);
+
+            }
+
         }
         else if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             transform.position += new Vector3(-1, 0, 0);
+            if (!CheckIsValidPosition())
+            {
+                transform.position += new Vector3(1, 0,0);
+
+            }
+
+
         }
         else if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-           transform.Rotate(0, 0, 90);
+            transform.Rotate(0, 0, 90);
+            if (!CheckIsValidPosition())
+            {
+                transform.position += new Vector3(0, 0,-90);
+
+            }
+
+
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow) || Time.time - fall >= fullspeed)
         {
             transform.position += new Vector3(0, -1, 0);
+            if (!CheckIsValidPosition())
+            {
+                transform.position += new Vector3(0, 1,0);
+
+            }
             fall = Time.time;
         }
     }
+
+    bool CheckIsValidPosition()
+    {
+        foreach (Transform mino in transform)
+        {
+            Vector2 pos = FindObjectOfType<Game>().Round(mino.position);
+            if (FindObjectOfType<Game>().CheckIsInsideGrid(pos) == false)
+            {
+                return false;
+            }
+
+        }
+
+        return true;
+    }
 }
+
+
